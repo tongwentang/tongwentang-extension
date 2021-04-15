830aa2e99a4cb4b28c99c4eccb322481cd1d1ef3
t7yang <t7yang@gmail.com>
2021-04-15 11:16:59 +0800

    fix: Make sure notify after convert clipboard finish.

--- src/background/clipboard/index.ts
+++ src/background/clipboard/index.ts
@@ -1,4 +1,3 @@
-import { tap } from 'ramda';
 import { LangType } from 'tongwen-core';
 import { i18n } from '../../service/i18n/i18n';
 import { createNoti } from '../../service/notification/create-noti';
@@ -11,11 +10,10 @@ const convertClipboardContent = (state: BgState, target: LangType): Promise<void
     .then(text => state.converter.phrase(target, text))
     .then(text => navigator.clipboard.writeText(text));
 
-// TODO: i18n
 export const convertClipboard = (state: BgState, target: LangType): Promise<void> =>
   permissions
     .request({ permissions: ['clipboardRead', 'clipboardWrite'] })
-    .then(tap(isGet => isGet && convertClipboardContent(state, target)))
+    .then(async isGet => (isGet && (await convertClipboardContent(state, target)), isGet))
     .then(isGet => {
       createNoti(
         i18n.getMessage(!isGet ? 'NT_GRT_PRM_DENIED' : target === LangType.s2t ? 'NT_CLB_TO_S2T' : 'NT_CLB_TO_T2S'),
