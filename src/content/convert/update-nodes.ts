36270a00a4d6f5992b555a1353ac3827667c550d
t7yang <t7yang@gmail.com>
2022-08-28 03:34:53 +0800

    fix: avoid to update node content if the converted content is same as original
    close Avoid replace text node with same value #39

--- src/content/convert/update-nodes.ts
+++ src/content/convert/update-nodes.ts
@@ -3,10 +3,10 @@ import { ParsedResult } from 'tongwen-core';
 const updateNode = (parsed: ParsedResult, text: string) => {
   switch (parsed.type) {
     case 'TEXT':
-      parsed.node.nodeValue = text;
+      parsed.node.nodeValue !== text && (parsed.node.nodeValue = text);
       break;
     case 'ELEMENT':
-      parsed.node.setAttribute(parsed.attr, text);
+      parsed.node.getAttribute(parsed.attr) !== text && parsed.node.setAttribute(parsed.attr, text);
   }
 };
 
