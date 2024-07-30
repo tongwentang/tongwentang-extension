349e897c253713a5aaf1db6569de7d00bb2c94b5
t7yang <t7yang@gmail.com>
2024-07-30 13:09:29 +0800

    fix: unable to add filter rule from browser action menu
    resolve #41, resolve #65

--- src/service/storage/local.ts
+++ src/service/storage/local.ts
@@ -7,11 +7,11 @@ export const patchLocalStorage = (reducer: StoreReducer): Promise<void> =>
   storage.get().then(reducer).then(storage.set);
 
 export const addFilterRule = (rule: PrefFilterRule): Promise<void> => {
-  return patchLocalStorage(({ filter: { enabled, rules } }) => ({
+  return patchLocalStorage(({ filter: { rules, ...rest } }) => ({
     filter: {
-      enabled,
+      ...rest,
       rules: (index => (index === -1 ? [rule, ...rules] : Object.assign([...rules], { [index]: rule })))(
-        rules.findIndex(rule => rule.pattern === rule.pattern),
+        rules.findIndex(r => r.pattern === rule.pattern),
       ),
     },
   }));
