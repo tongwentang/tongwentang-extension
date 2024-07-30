898c30e78ab4d72aa8f532037c469a27c0e95cb0
t7yang <t7yang@gmail.com>
2024-07-30 13:34:27 +0800

    fix: should escape regex keyword before convert

--- src/preference/filter-rule/index.ts
+++ src/preference/filter-rule/index.ts
@@ -2,16 +2,14 @@ import { getRandomId } from '../../utilities';
 import { PrefFilterRule, RegExpMaybe } from '../types/v2';
 
 export const REGEXP_PATTERN = /^\/(.+)\/([gimuy]{0,5})$/;
-
 export const DOMAIN_PATTERN = /^[\w-]+\.([\w-]+\.)*[\w-]+$/;
 
 export const isUrlLike = (pattern: string) => /https?:(\/?\/?)[^\s]+/.test(pattern);
-
 export const isDomainLike = (pattern: string) => DOMAIN_PATTERN.test(pattern);
-
 export const isRegExpLike = (pattern: string) => REGEXP_PATTERN.test(pattern);
-
 export const isFilterPatternValid = (pattern: string) => isDomainLike(pattern) || isRegExpLike(pattern);
+
+const escapeRegex = (str: string): string => str.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 
 const createRegExpWithRegexLike = (pattern: string): RegExpMaybe => {
   try {
@@ -24,7 +22,7 @@ const createRegExpWithRegexLike = (pattern: string): RegExpMaybe => {
 
 const createRegExpWithDomainLike = (pattern: string): RegExpMaybe => {
   try {
-    return new RegExp(pattern);
+    return new RegExp(escapeRegex(pattern));
   } catch (error) {
     return null;
   }
